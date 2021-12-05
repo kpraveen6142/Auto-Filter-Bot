25710cc6891152bf5612dc2b88eff0278000b82a
kpraveen6142 <93444100+kpraveen6142@users.noreply.github.com>
2021-12-05 10:50:14 +0530

    Create filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -174,7 +174,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("💟 SUPPORT 💟", url="https://t.me/ThiruttuRockers4k")]
+                [InlineKeyboardButton("💟 SUPPORT 💟", url="https://t.me/groupdc")]
             ])
 
             await query.message.edit_text(
